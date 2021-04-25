2769d740c9d6b219fe7e56c2cba9d255ba1aca30
Aditya Singh <karateilove@gmail.com>
2021-04-25 20:01:59 +0530

    call by name and call by value illustration

---
+++ src/LearningScalaCode/Setion_1_Basic/CBNandCBV.scala
@@ -0,0 +1,17 @@
+package LearningScalaCode.Setion_1_Basic
+
+object CBNandCBV extends App{
+  def calledbyValue(x:Long):Unit={
+    println("By value call==>"+ x)
+    println("By value call==>"+ x)
+  }
+  def calledbyName(x: => Long):Unit={//For doing by name => is used
+    println("By name call==>"+ x)
+    println("By name call==>"+ x)
+  }
+
+  (calledbyValue(System.nanoTime()))//The value of System.nanoTime is calculated first and value calculated is sent to function
+  (calledbyName(System.nanoTime()))//Here the expression System.nanoTime is sent to function and hence you see 2 different time
+
+
+}
